9c7ad8082bba28470ab36a99b73e90f72bad69b1
Mohammed Sameer <sameer@Mohammeds-MacBook-Air.local>
2025-08-04 11:53:28 +0530

    add the backend to store the state file

--- provider.tf
+++ provider.tf
@@ -5,6 +5,12 @@ terraform {
       version = "6.6.0"
     }
   }
+  backend "s3" {
+    bucket         = "my-terraform-state-file-backend"
+    key            = "network/terraform.tfstate"
+    region         = "ap-south-1"
+    dynamodb_table = "terraform-locks"
+  }
 }
 
 provider "aws" {
